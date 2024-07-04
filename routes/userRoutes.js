e584b26e4077a4e174968b3be2574e6be1b9f158
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-07-04 16:54:26 +0100

    merge les conflits

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/usercontroller');
+const userController = require('../controller/userController');
 
 
 
